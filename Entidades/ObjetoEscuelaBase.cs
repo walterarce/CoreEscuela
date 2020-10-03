e5c7f88167d0b71e7cd7b5cc099ed3232c7568f7
Walter Arce <walterarce@gmail.com>
2020-10-03 16:36:54 -0300

    subo version limpia con ignore nuevo

---
+++ Entidades/ObjetoEscuelaBase.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace CoreEscuela.Entidades
+{
+    public  class  ObjetoEscuelaBase
+    {
+           string nombre;
+        
+        public string UniqueId { get; private set; } 
+         public string Nombre { get {return "Copia:" +nombre;} set {nombre = value.ToUpper();} }
+        public ObjetoEscuelaBase()
+        {
+                UniqueId  = Guid.NewGuid().ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"{Nombre},{UniqueId}";
+        }
+    }
+}
